2be106ceedc3139b7fc92fdefec73a702833a110
Christian Jarvers <christian.jarvers@gmx.de>
2023-04-19 12:44:28 +0200

    Streamlined training and generalization tests

--- experiments/trainlines.py
+++ experiments/trainlines.py
@@ -7,13 +7,18 @@ sys.path.append("..")
 from shapelearningtheory.linedataset import LineDataModule
 from shapelearningtheory.linearnetworks import ShallowLinear, DeepLinear
 from shapelearningtheory.mlp import MLP
-from shapelearningtheory.colorcategories import White
+from shapelearningtheory.colorcategories import White, Grey, RedXORBlue, NotRedXORBlue
 
-# get data
-traindata = LineDataModule(15, 15, range(5, 11))
-shorttest = LineDataModule(15, 15, [3])
-longtest = LineDataModule(15, 15, [13])
-whitetest = LineDataModule(15, 15, [7], horizontalcolor=White, verticalcolor=White)
+# get data:
+# training dataset
+traindata = LineDataModule(15, 15, range(5, 11), horizontalcolor=RedXORBlue, verticalcolor=NotRedXORBlue)
+# test datasets - parametrized slightly differently to test generalization
+test_sets = {
+    "short": LineDataModule(15, 15, [3], horizontalcolor=RedXORBlue, verticalcolor=NotRedXORBlue), # shorter lines, correct color
+    "long": LineDataModule(15, 15, [13], horizontalcolor=RedXORBlue, verticalcolor=NotRedXORBlue), # longer lines, correct color
+    "grey": LineDataModule(15, 15, [7], horizontalcolor=Grey, verticalcolor=Grey), # medium length lines, no color
+    "conflict": LineDataModule(15, 15, [7], horizontalcolor=NotRedXORBlue, verticalcolor=RedXORBlue) # medium length lines, incorrect color
+}
 
 # define models
 shallow_model = ShallowLinear(15 * 15 * 3, 2, loss_fun=torch.nn.functional.cross_entropy, 
@@ -24,24 +29,37 @@ deep_model = DeepLinear(num_inputs=15 * 15 * 3, num_hidden=1000, num_layers=3,
 mlp_model = MLP(num_inputs=15 * 15 * 3, num_hidden=1000, num_layers=3,
     num_outputs=2, loss_fun=torch.nn.functional.cross_entropy, 
     metric=Accuracy("multiclass", num_classes=2))
+models = {
+    "shallow_linear": shallow_model,
+    "deep_linear": deep_model,
+    "mlp": mlp_model
+}
 
 # initialize trainers
-shallow_trainer = pl.Trainer(max_epochs=100)
-deep_trainer = pl.Trainer(max_epochs=100)
-mlp_trainer = pl.Trainer(max_epochs=100)
+trainers = {name: pl.Trainer(max_epochs=100) for name in models.keys()}
 
 # train
-shallow_trainer.fit(shallow_model, traindata)
-deep_trainer.fit(deep_model, traindata)
-mlp_trainer.fit(mlp_model, traindata)
+for name, model in models.items():
+    trainers[name].fit(model, traindata)
+
 
 # test generalization with shorter and longer lines
-shallow_trainer.test(shallow_model, shorttest)
-shallow_trainer.test(shallow_model, longtest)
-shallow_trainer.test(shallow_model, whitetest)
-deep_trainer.test(deep_model, shorttest)
-deep_trainer.test(deep_model, longtest)
-deep_trainer.test(deep_model, whitetest)
-mlp_trainer.test(mlp_model, shorttest)
-mlp_trainer.test(mlp_model, longtest)
-mlp_trainer.test(mlp_model, whitetest)
+test_results = {}
+for name, model in models.items():
+    results = {}
+    for testname, testset in test_sets.items():
+        results[testname] = trainers[name].test(model, testset, verbose=False)
+    test_results[name] = results
+# Print test results as table
+cellwidth = 15
+print("Test results:")
+print("| " + "Network".ljust(cellwidth-1), end="|")
+for testname in test_sets.keys():
+    print(" " + testname.ljust(cellwidth-1), end="|")
+print("\n" + ("|" + "-" * cellwidth) * (len(test_sets)+1) + "|")
+for model, results in test_results.items():
+    print("| " + model.ljust(cellwidth-1), end="")
+    for testname, result in results.items():
+        r = round(result[0]["test_metric"], ndigits=3)
+        print("|" + f"{r}".rjust(cellwidth-2), end="  ")
+    print("|")
