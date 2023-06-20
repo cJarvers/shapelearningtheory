4e62ae1036dc92596e711c1baffe882cf1e47f48
Christian Jarvers <christian.jarvers@gmx.de>
2023-06-20 17:47:21 +0200

    Add dataset with rectangles

---
+++ experiments/trainrectangles.py
@@ -0,0 +1,85 @@
+import torch
+from torchmetrics import Accuracy
+import pytorch_lightning as pl
+import sys
+# local imports
+sys.path.append("..")
+from shapelearningtheory.rectangledataset import ColorRectangleDataModule
+from shapelearningtheory.squaredataset import SquaresDataModule
+from shapelearningtheory.linearnetworks import ShallowLinear, DeepLinear
+from shapelearningtheory.mlp import MLP
+from shapelearningtheory.convnet import SimpleConvNet
+from shapelearningtheory.colorcategories import Grey, RedXORBlue, NotRedXORBlue, RandomRed, RandomBlue
+
+# hyper-parameters for the task
+color1 = RedXORBlue # RandomRed # 
+color2 = NotRedXORBlue # RandomBlue # 
+imgsize = 15
+minlength = 7
+maxlength = 13
+minwidth = 4
+maxwidth = 8
+# hyper-parameters for the networks
+num_layers = 3
+num_hidden = 1000
+# hyper-parameters for training
+epochs = 100
+
+# get data:
+# training dataset
+traindata = ColorRectangleDataModule(imgsize, imgsize, range(minlength, maxlength), range(minwidth, maxwidth), color1=color1, color2=color2)
+#
+# test datasets - parametrized slightly differently to test generalization
+test_sets = {
+    "color only": SquaresDataModule(imgsize, imgsize, range(minwidth, maxlength), color1=color1, color2=color2), # correct color, but squares instead of rectangles (cannot classify by shape)
+    "shape only": ColorRectangleDataModule(imgsize, imgsize, range(minlength, maxlength), range(minwidth, maxwidth), color1=Grey, color2=Grey), # same rectangles but no color
+    "conflict": ColorRectangleDataModule(imgsize, imgsize, range(minlength, maxlength), range(minwidth, maxwidth), color1=color2, color2=color1) # same rectangles, incorrect color
+}
+
+# define models
+shallow_model = ShallowLinear(imgsize * imgsize * 3, 2, loss_fun=torch.nn.functional.cross_entropy, 
+    metric=Accuracy("multiclass", num_classes=2))
+deep_model = DeepLinear(num_inputs=imgsize * imgsize * 3, num_hidden=num_hidden, num_layers=num_layers,
+    num_outputs=2, loss_fun=torch.nn.functional.cross_entropy, 
+    metric=Accuracy("multiclass", num_classes=2))
+mlp_model = MLP(num_inputs=imgsize * imgsize * 3, num_hidden=num_hidden, num_layers=num_layers,
+    num_outputs=2, loss_fun=torch.nn.functional.cross_entropy, 
+    metric=Accuracy("multiclass", num_classes=2))
+simpleconv_model = SimpleConvNet(channels_per_layer=[16, 32, 64], kernel_sizes=[3,3,3],
+    in_channels=3, out_units=2, loss_fun=torch.nn.functional.cross_entropy, 
+    metric=Accuracy("multiclass", num_classes=2))
+models = {
+    "shallow_linear": shallow_model,
+    "deep_linear": deep_model,
+    "mlp": mlp_model,
+    "conv": simpleconv_model
+}
+
+# initialize trainers
+trainers = {name: pl.Trainer(max_epochs=epochs) for name in models.keys()}
+
+# train
+for name, model in models.items():
+    trainers[name].fit(model, traindata)
+
+
+# test generalization with shorter and longer lines
+test_results = {}
+for name, model in models.items():
+    results = {}
+    for testname, testset in test_sets.items():
+        results[testname] = trainers[name].test(model, testset, verbose=False)
+    test_results[name] = results
+# Print test results as table
+cellwidth = 15
+print("Test results:")
+print("| " + "Network".ljust(cellwidth-1), end="|")
+for testname in test_sets.keys():
+    print(" " + testname.ljust(cellwidth-1), end="|")
+print("\n" + ("|" + "-" * cellwidth) * (len(test_sets)+1) + "|")
+for model, results in test_results.items():
+    print("| " + model.ljust(cellwidth-1), end="")
+    for testname, result in results.items():
+        r = round(result[0]["test_metric"], ndigits=3)
+        print("|" + f"{r}".rjust(cellwidth-2), end="  ")
+    print("|")
---
+++ shapelearningtheory/rectangledataset.py
@@ -0,0 +1,106 @@
+import torch
+from torch.utils.data import Dataset, random_split, DataLoader
+from typing import Any, List, Type
+from pytorch_lightning import LightningDataModule
+# local imports
+from .colorcategories import Color, RandomRed, RandomBlue
+from .shapecategories import ColorRectangle, Pixel, Orientation
+
+class ColorRectangleDataset(Dataset):
+    """
+    Dataset for classifying recangles according to orientation (whether they are
+    higher than wide or wider than high) and color.
+
+    Args:
+        - imgheight: int - height of output images
+        - imgwidth: int - width of output images
+        - lenghts: List[int] - lengths of longer sides for which to generate rectangles
+        - widths: List[int] - lengths of shorter sides for which to generate rectangles
+        - color1: Type[Color] - color type for class 1
+        - color2: Type[Color] - color type for class 2
+    """
+    def __init__(self, imgheight: int, imgwidth: int, lengths: List[int],
+            widths: List[int], color1: Type[Color], color2: Type[Color]):
+        super().__init__()
+        # store parameters
+        self.imgheight = imgheight
+        self.imgwidth = imgwidth
+        self.lengths = lengths
+        self.widths = widths
+        self.color1 = color1
+        self.color2 = color2
+        # generate dataset
+        self.rectangles = self.generate_all_rectangles()
+
+    def generate_all_rectangles(self):
+        horizontal = []
+        vertical = []
+        for l in self.lengths:
+            for w in self.widths:
+                if w < l:
+                    for x in range(self.imgheight):
+                        for y in range(self.imgwidth):
+                            horizontal.append(
+                                ColorRectangle(
+                                    start=Pixel(x, y),
+                                    length=l,
+                                    width=w,
+                                    orientation=Orientation.HORIZONTAL,
+                                    color=self.color1()
+                                )
+                            )
+                            vertical.append(
+                                ColorRectangle(
+                                    start=Pixel(x, y),
+                                    length=l,
+                                    width=w,
+                                    orientation=Orientation.VERTICAL,
+                                    color=self.color2()
+                                )
+                            )
+        return horizontal + vertical
+    
+    def __getitem__(self, index: int) -> Any:
+        rectangle = self.rectangles[index]
+        if rectangle.orientation == Orientation.HORIZONTAL:
+            label = 0
+        else:
+            label = 1
+        image = torch.zeros(self.imgwidth, self.imgheight, 3)
+        rectangle.draw_to_tensor(image)
+        return image.permute(2,0,1), label
+    
+    def __len__(self):
+        return len(self.rectangles)
+
+
+class ColorRectangleDataModule(LightningDataModule):
+    def __init__(self, imgheight:int, imgwidth: int, lengths: List[int],
+            widths: List[int], batch_size: int = 32, num_workers: int = 4,
+            color1: Type[Color] = RandomRed,
+            color2: Type[Color] = RandomBlue):
+        super().__init__()
+        self.lengths = lengths
+        self.widths = widths
+        self.color1 = color1
+        self.color2 = color2
+        self.save_hyperparameters(ignore=["lengths", "widths"])
+
+    def prepare_data(self) -> None:
+        self.dataset = ColorRectangleDataset(
+            self.hparams.imgheight, self.hparams.imgwidth, self.lengths,
+            self.widths, self.color1, self.color2
+        )
+        self.train, self.val = random_split(self.dataset, [0.5, 0.5])
+
+    def train_dataloader(self) -> Any:
+        return DataLoader(self.train, self.hparams.batch_size, shuffle=True,
+            num_workers=self.hparams.num_workers)
+    
+    def val_dataloader(self) -> Any:
+        return DataLoader(self.val, self.hparams.batch_size, shuffle=False,
+            num_workers=self.hparams.num_workers)
+    
+    def test_dataloader(self) -> Any:
+        return DataLoader(self.dataset, self.hparams.batch_size, shuffle=False,
+            num_workers=self.hparams.num_workers)
