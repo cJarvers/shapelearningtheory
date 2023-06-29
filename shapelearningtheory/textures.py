3b54bb6512358a0b5f268286ef6efc49b3c5df7b
Christian Jarvers <christian.jarvers@gmx.de>
2023-06-29 11:31:09 +0200

    Added horizontal and vertical sine grating texture classes

--- shapelearningtheory/textures.py
+++ shapelearningtheory/textures.py
@@ -1,3 +1,4 @@
+from math import sin, cos
 import torch
 from torch import Tensor
 
@@ -15,11 +16,33 @@ class Texture:
         pattern = self.value_at_coordinate(grid_x, grid_y)
         return pattern
 
-class SineGrating:
+class SineGrating(Texture):
     """
-    
+    Base class for sine gratings. Not probabilistic, so it should not be used as a
+    pattern class.
     """
     def __init__(self, frequency, orientation, phase):
         self.frequency = frequency
         self.orientation = orientation
         self.phase = phase
+
+    def value_at_coordinate(self, x, y, channels=3):
+        x_prime = x * cos(self.orientation) + y * sin(self.orientation)
+        value = torch.sin(2 * torch.pi * self.frequency * x_prime + self.phase)
+        return value
+    
+class HorizontalGrating(SineGrating):
+    """Horizontal grating with somewhat randomized frequency, phase, and orientation."""
+    def __init__(self, frequency_min=0.1, frequency_max=0.3, orientation_range=torch.pi / 5):
+        frequency = torch.rand(1).item() * (frequency_max - frequency_min) + frequency_min
+        orientation = torch.rand(1).item() * (2 * orientation_range) - orientation_range
+        phase = torch.rand(1).item() / frequency
+        super().__init__(frequency, orientation, phase)
+
+class VerticalGrating(SineGrating):
+    """Horizontal grating with somewhat randomized frequency, phase, and orientation."""
+    def __init__(self, frequency_min=0.1, frequency_max=0.3, orientation_range=torch.pi / 5):
+        frequency = torch.rand(1).item() * (frequency_max - frequency_min) + frequency_min
+        orientation = torch.pi / 2 + torch.rand(1).item() * (2 * orientation_range) - orientation_range
+        phase = torch.rand(1).item() / frequency
+        super().__init__(frequency, orientation, phase)
