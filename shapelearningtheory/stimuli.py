2ae13023be63feae43b196b1b7faec99c13c76b8
Christian Jarvers <christian.jarvers@gmx.de>
2023-07-03 13:32:30 +0200

    Added option to re-scale stimuli to range [-1, 1]

--- shapelearningtheory/stimuli.py
+++ shapelearningtheory/stimuli.py
@@ -7,11 +7,13 @@ class Stimulus:
     """Combination of a shape and a color or texture.
     Used to generate the actual stimulus picture."""
     def __init__(self, shape: Shape, pattern: Color | Texture,
-            background_pattern: Optional[Color | Texture] = None, wrap: bool=True):
+            background_pattern: Optional[Color | Texture] = None,
+            wrap: bool=True, correct_range: bool=True):
         self.shape = shape
         self.pattern = pattern
         self.background_pattern = background_pattern
         self.wrap = wrap
+        self.correct_range = correct_range
 
     def create_image(self, height, width):
         mask = self.shape.generate_mask(height, width, self.wrap)
@@ -21,4 +23,7 @@ class Stimulus:
             stimulus = mask * image + mask.logical_not() * background
         else:
             stimulus = mask * image
+        if self.correct_range:
+            # images are created in range [0, 1]; change to range [-1, 1] instead (better for training)
+            stimulus = 2 * stimulus - 1
         return stimulus
