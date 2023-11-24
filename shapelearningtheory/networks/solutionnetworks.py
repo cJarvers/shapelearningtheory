6dec5d87b1b274f99b28b15bd0847c6557b7adbb
Christian Jarvers <christian.jarvers@gmx.de>
2023-11-24 15:26:48 +0100

    Refactored layers for constructed networks.

--- shapelearningtheory/networks/solutionnetworks.py
+++ shapelearningtheory/networks/solutionnetworks.py
@@ -2,8 +2,24 @@
 import numpy as np
 import torch
 from skimage.filters import gabor_kernel
+from collections import OrderedDict
 
-class ColorConvNet(torch.nn.Sequential):
+class FixedSequential(torch.nn.Sequential):
+    """Sequential model that has a fixed structure (constructor does not take arguments),
+    but that can still be subscripted correctly."""
+    def __init__(self):
+        super().__init__()
+
+    def __getitem__(self, idx):
+        if isinstance(idx, slice):
+            return torch.nn.Sequential(OrderedDict(list(self._modules.items())[idx]))
+        else:
+            return self._get_item_by_idx(self._modules.values(), idx)
+
+############
+# Networks #
+############
+class ColorConvNet(FixedSequential):
     """Convolutional network that classifies RedXORBlue vs. NotRedXORBlue by construction."""
     def __init__(self, imageheight: int, imagewidth: int,
                  min_pixels: int = 35, max_pixels: int = 117):
@@ -40,56 +56,17 @@ class ColorConvNet(torch.nn.Sequential):
         return layer
 
 
-class CRectangleConvNet(torch.nn.Sequential):
+class CRectangleConvNet(FixedSequential):
     """ConvNet that classifies color rectangles by height / width."""
     def __init__(self):
         super().__init__()
-        self.append(self.sobel_layer())
+        self.append(SobelLayer())
         self.append(torch.nn.ReLU())
-        self.append(self.borders_layer())
+        self.append(SumChannels(12, 2))
         self.append(self.distance_layer())
         self.append(torch.nn.AdaptiveMaxPool2d(output_size=1))
         self.append(torch.nn.Flatten())
         self.append(self.decision_layer())
-
-    def sobel_layer(self) -> torch.nn.Conv2d:
-        """Create layer that performs Sobel filtering to detect horzontal
-        and vertical contrasts in each color channel."""
-        # Detect horizontal and vertical contrast with sobel filters
-        sobel_x = torch.tensor([[-0.25, 0., 0.25], [-0.5, 0., 0.5], [-0.25, 0., 0.25]])
-        sobel_y = sobel_x.T
-        # Detect borders in each color channel for both possible polarities and both orientations: 12 channels
-        conv_borders = torch.nn.Conv2d(3, 12, 3)
-        conv_borders.weight.data.zero_()
-        conv_borders.bias.data.zero_()
-        # Set sobel filters:
-        conv_borders.weight.data[0, 0, :, :] = sobel_x
-        conv_borders.weight.data[1, 0, :, :] = -sobel_x
-        conv_borders.weight.data[2, 1, :, :] = sobel_x
-        conv_borders.weight.data[3, 1, :, :] = -sobel_x
-        conv_borders.weight.data[4, 2, :, :] = sobel_x
-        conv_borders.weight.data[5, 2, :, :] = -sobel_x
-        conv_borders.weight.data[6, 0, :, :] = sobel_y
-        conv_borders.weight.data[7, 0, :, :] = -sobel_y
-        conv_borders.weight.data[8, 1, :, :] = sobel_y
-        conv_borders.weight.data[9, 1, :, :] = -sobel_y
-        conv_borders.weight.data[10, 2, :, :] = sobel_y
-        conv_borders.weight.data[11, 2, :, :] = -sobel_y
-        # use a slightly negative bias to suppress weak responses
-        conv_borders.bias.data[:] = -0.1
-        return conv_borders
-    
-    def borders_layer(self) -> torch.nn.Conv2d:
-        # This layer sums up signals for edges across colors and polarities,
-        # but keeps the 2 orientations in different channels
-        conv_bordertypes = torch.nn.Conv2d(12, 2, 1)
-        conv_bordertypes.bias.data.zero_()
-        conv_bordertypes.weight.data.zero_()
-        # The first channel sums up all vertical edges
-        conv_bordertypes.weight.data[0, 0:6, :, :] = 1.0
-        # The second channels sums up all horizontal edges
-        conv_bordertypes.weight.data[1, 6:12, :, :] = 1.0
-        return conv_bordertypes
     
     def distance_layer(self) -> torch.nn.Conv2d:
         # This layer calculates the distance from
@@ -127,59 +104,140 @@ class CRectangleConvNet(torch.nn.Sequential):
         layer.weight.data[1] = torch.tensor([0.0, 0.0, 1.0, 1.0])
         return layer
     
-class TextureConvNet(torch.nn.Sequential):
+class SRectangleConvNet(FixedSequential):
+    """ConvNet that classifies striped rectangles by shape."""
+    def __init__(self):
+        super().__init__()
+    
+class TextureConvNet(FixedSequential):
     """Convolutional network that classifies input images based on
     whether they contain a horizontal or vertical texture."""
     def __init__(self):
         super().__init__()
-        self.append(self.gabor_layer())
+        self.append(GaborLayer())
         self.append(torch.nn.ReLU())
-        self.append(self.summation_layer())
+        self.append(SumChannels(24, 2))
         self.append(torch.nn.AdaptiveMaxPool2d(output_size=1))
         self.append(torch.nn.Flatten())
 
-    @torch.no_grad()
-    def gabor_layer(self) -> torch.nn.Conv2d:
-        horizontal_gabor = gabor_kernel(frequency=0.1, theta=np.pi/2, bandwidth=3)
-        vertical_gabor = gabor_kernel(frequency=0.1, theta=0, bandwidth=3)
-        kernel_size = horizontal_gabor.shape
-        layer = torch.nn.Conv2d(3, 24, kernel_size, bias=False, padding="same",
-            padding_mode="replicate")
-        layer.weight.data.zero_()
-        layer.weight[ 0, 0, :, :] = torch.from_numpy(  horizontal_gabor.real - horizontal_gabor.real.mean())
-        layer.weight[ 1, 0, :, :] = torch.from_numpy(- horizontal_gabor.real + horizontal_gabor.real.mean())
-        layer.weight[ 2, 0, :, :] = torch.from_numpy(  horizontal_gabor.imag)
-        layer.weight[ 3, 0, :, :] = torch.from_numpy(- horizontal_gabor.imag)
-        layer.weight[ 4, 1, :, :] = torch.from_numpy(  horizontal_gabor.real - horizontal_gabor.real.mean())
-        layer.weight[ 5, 1, :, :] = torch.from_numpy(- horizontal_gabor.real + horizontal_gabor.real.mean())
-        layer.weight[ 6, 1, :, :] = torch.from_numpy(  horizontal_gabor.imag)
-        layer.weight[ 7, 1, :, :] = torch.from_numpy(- horizontal_gabor.imag)
-        layer.weight[ 8, 2, :, :] = torch.from_numpy(  horizontal_gabor.real - horizontal_gabor.real.mean())
-        layer.weight[ 9, 2, :, :] = torch.from_numpy(- horizontal_gabor.real + horizontal_gabor.real.mean())
-        layer.weight[10, 2, :, :] = torch.from_numpy(  horizontal_gabor.imag)
-        layer.weight[11, 2, :, :] = torch.from_numpy(- horizontal_gabor.imag)
-        layer.weight[12, 0, :, :] = torch.from_numpy(  vertical_gabor.real - vertical_gabor.real.mean())
-        layer.weight[13, 0, :, :] = torch.from_numpy(- vertical_gabor.real + vertical_gabor.real.mean())
-        layer.weight[14, 0, :, :] = torch.from_numpy(  vertical_gabor.imag)
-        layer.weight[15, 0, :, :] = torch.from_numpy(- vertical_gabor.imag)
-        layer.weight[16, 1, :, :] = torch.from_numpy(  vertical_gabor.real - vertical_gabor.real.mean())
-        layer.weight[17, 1, :, :] = torch.from_numpy(- vertical_gabor.real + vertical_gabor.real.mean())
-        layer.weight[18, 1, :, :] = torch.from_numpy(  vertical_gabor.imag)
-        layer.weight[19, 1, :, :] = torch.from_numpy(- vertical_gabor.imag)
-        layer.weight[20, 2, :, :] = torch.from_numpy(  vertical_gabor.real - vertical_gabor.real.mean())
-        layer.weight[21, 2, :, :] = torch.from_numpy(- vertical_gabor.real + vertical_gabor.real.mean())
-        layer.weight[22, 2, :, :] = torch.from_numpy(  vertical_gabor.imag)
-        layer.weight[23, 2, :, :] = torch.from_numpy(- vertical_gabor.imag)
-        return layer
-    
-    def summation_layer(self):
-        layer = torch.nn.Conv2d(
-            in_channels=24,
-            out_channels=2,
-            kernel_size=1,
-            bias=False
+class CLTConvNet(FixedSequential):
+    """ConvNet that classifies colorful L and T shapes by shape."""
+    def __init__(self):
+        super().__init__()
+
+#################
+# Helper layers #
+#################
+class LaplaceLayer(torch.nn.Conv2d):
+    """Convolution layer that performs Laplace filtering."""
+    def __init__(self, in_channels=3):
+        super().__init__(
+            in_channels = in_channels,
+            out_channels = 2,
+            kernel_size = 3,
+            bias = True,
+            padding = "same",
+            padding_mode = "replicate"
         )
-        layer.weight.data.zero_()
-        layer.weight.data[0, 0:12] = 1.0
-        layer.weight.data[1, 12:24] = 1.0
-        return layer
+        with torch.no_grad():
+            self.bias.data[:] = -0.01
+            laplace_kernel = torch.tensor(
+                [[0, 1, 0],
+                 [1, -4, 1],
+                 [0, 1, 0]]
+            )
+            self.weight.data[0, :, :, :] = laplace_kernel
+            self.weight.data[1, :, :, :] = -laplace_kernel
+
+
+class Heaviside(torch.nn.Module):
+    """Heaviside activation function."""
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return x.heaviside(values=torch.zeros_like(x))
+
+class GaborLayer(torch.nn.Conv2d):
+    """Convolution layer that is initialized with a Gabor filter bank."""
+    def __init__(self, frequency=0.1, bandwidth=3):
+        horizontal_gabor = gabor_kernel(
+            frequency=frequency, theta=np.pi/2, bandwidth=bandwidth)
+        vertical_gabor = gabor_kernel(
+            frequency=frequency, theta=0, bandwidth=bandwidth)
+        kernel_size = horizontal_gabor.shape
+        super().__init__(
+            in_channels=3,
+            out_channels=24,
+            kernel_size=kernel_size,
+            bias=False, 
+            padding="same",
+            padding_mode="replicate"
+        )
+        with torch.no_grad():
+            self.weight.data.zero_()
+            self.weight[ 0, 0, :, :] = torch.from_numpy(  horizontal_gabor.real - horizontal_gabor.real.mean())
+            self.weight[ 1, 0, :, :] = torch.from_numpy(- horizontal_gabor.real + horizontal_gabor.real.mean())
+            self.weight[ 2, 0, :, :] = torch.from_numpy(  horizontal_gabor.imag)
+            self.weight[ 3, 0, :, :] = torch.from_numpy(- horizontal_gabor.imag)
+            self.weight[ 4, 1, :, :] = torch.from_numpy(  horizontal_gabor.real - horizontal_gabor.real.mean())
+            self.weight[ 5, 1, :, :] = torch.from_numpy(- horizontal_gabor.real + horizontal_gabor.real.mean())
+            self.weight[ 6, 1, :, :] = torch.from_numpy(  horizontal_gabor.imag)
+            self.weight[ 7, 1, :, :] = torch.from_numpy(- horizontal_gabor.imag)
+            self.weight[ 8, 2, :, :] = torch.from_numpy(  horizontal_gabor.real - horizontal_gabor.real.mean())
+            self.weight[ 9, 2, :, :] = torch.from_numpy(- horizontal_gabor.real + horizontal_gabor.real.mean())
+            self.weight[10, 2, :, :] = torch.from_numpy(  horizontal_gabor.imag)
+            self.weight[11, 2, :, :] = torch.from_numpy(- horizontal_gabor.imag)
+            self.weight[12, 0, :, :] = torch.from_numpy(  vertical_gabor.real - vertical_gabor.real.mean())
+            self.weight[13, 0, :, :] = torch.from_numpy(- vertical_gabor.real + vertical_gabor.real.mean())
+            self.weight[14, 0, :, :] = torch.from_numpy(  vertical_gabor.imag)
+            self.weight[15, 0, :, :] = torch.from_numpy(- vertical_gabor.imag)
+            self.weight[16, 1, :, :] = torch.from_numpy(  vertical_gabor.real - vertical_gabor.real.mean())
+            self.weight[17, 1, :, :] = torch.from_numpy(- vertical_gabor.real + vertical_gabor.real.mean())
+            self.weight[18, 1, :, :] = torch.from_numpy(  vertical_gabor.imag)
+            self.weight[19, 1, :, :] = torch.from_numpy(- vertical_gabor.imag)
+            self.weight[20, 2, :, :] = torch.from_numpy(  vertical_gabor.real - vertical_gabor.real.mean())
+            self.weight[21, 2, :, :] = torch.from_numpy(- vertical_gabor.real + vertical_gabor.real.mean())
+            self.weight[22, 2, :, :] = torch.from_numpy(  vertical_gabor.imag)
+            self.weight[23, 2, :, :] = torch.from_numpy(- vertical_gabor.imag)
+
+class SobelLayer(torch.nn.Conv2d):
+    """Convolution layer that performs Sobel filtering in x and y direction."""
+    def __init__(self, in_channels=3):
+        super().__init__(
+            in_channels = in_channels,
+            out_channels = 4 * in_channels,
+            kernel_size = 3,
+            bias=True,
+            padding="same",
+            padding_mode="replicate"
+        )
+        sobel_x = torch.tensor([[-0.25, 0., 0.25], [-0.5, 0., 0.5], [-0.25, 0., 0.25]])
+        sobel_y = sobel_x.T
+        with torch.no_grad():
+            self.weight.data.zero_()
+            for i in range(in_channels):
+                self.weight.data[2*i, i, :, :] = sobel_x
+                self.weight.data[2*i+1, i, :, :] = -sobel_x
+                n = in_channels
+                self.weight.data[2*n+2*i, i, :, :] = sobel_y
+                self.weight.data[2*n+2*i+1, i, :, :] = -sobel_y
+            self.bias.data[:] = -0.1
+
+class SumChannels(torch.nn.Conv2d):
+    """1x1 convolution that sums over channels in equal groups."""
+    def __init__(self, in_channels: int, groups: int):
+        super().__init__(
+            in_channels = in_channels,
+            out_channels = groups,
+            kernel_size = 1,
+            bias = False,
+        )
+        assert(in_channels % groups == 0 and in_channels > groups)
+        channels_per_group = in_channels // groups
+        with torch.no_grad():
+            self.weight.data.zero_()
+            for i in range(groups):
+                start = i * channels_per_group
+                stop = start + channels_per_group
+                self.weight.data[i, start:stop, :] = 1.0
