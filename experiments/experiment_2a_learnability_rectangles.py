078956e41817d95a0eb416fa32eef5c12851731d
Christian Jarvers <christian.jarvers@gmx.de>
2023-12-08 21:18:15 +0100

    Adjusted scripts for experiment 2.

--- experiments/experiment_2a_learnability_rectangles.py
+++ experiments/experiment_2a_learnability_rectangles.py
@@ -3,7 +3,7 @@ import seaborn as sns
 import sys
 # local imports
 sys.path.append("..")
-from shapelearningtheory.datasets import make_rectangles_wrong_color, make_rectangles_shapeonly
+from shapelearningtheory.datasets import make_rectangles_color, make_rectangles_shapeonly, make_rectangles_texture
 from shapelearningtheory.networks import make_mlp_small, make_convnet_small, make_rconvnet_small, \
     make_softmaxconv_small, make_ViT_small, make_AE_small
 from helpers import print_table, train_and_validate, unpack_results
@@ -19,7 +19,8 @@ traindata = make_rectangles_shapeonly()
 test_sets = {
     "traindata": traindata,
     "validation": make_rectangles_shapeonly(),
-    "conflict": make_rectangles_wrong_color()
+    "with color": make_rectangles_color(),
+    "with texture": make_rectangles_texture()
 }
 
 # hyperparameters from dataset
@@ -34,16 +35,17 @@ models = {
     "mlp": lambda: make_mlp_small(num_inputs=imgheight * imgwidth * channels, num_outputs=classes),
     "conv": lambda: make_convnet_small(channels=channels, classes=classes),
     "rconv": lambda: make_rconvnet_small(channels=channels, classes=classes),
-    "softmaxconv": lambda: make_softmaxconv_small(channels=channels, classes=classes),
+    #"softmaxconv": lambda: make_softmaxconv_small(channels=channels, classes=classes),
     "ViT": lambda: make_ViT_small(imgsize=imgheight, classes=classes),
-    "autoencoder": lambda: make_AE_small(num_inputs=imgheight * imgwidth * channels, classes=classes)
+    #"autoencoder": lambda: make_AE_small(num_inputs=imgheight * imgwidth * channels, classes=classes)
 }
 
 # train and test
 test_results = {}
 for name, model in models.items():
     test_results[name] = train_and_validate(
-        model, traindata, test_sets
+        model, traindata, test_sets,
+        epochs=epochs
     )
 
 # Print test results as table
