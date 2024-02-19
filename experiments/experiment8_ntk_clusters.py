de855e846f8c3e1fd667b4a366a7c5de8d47e107
Christian Jarvers <christian.jarvers@gmx.de>
2024-02-19 14:30:47 +0100

    Changed clustering back to same test set as experiment 7.

--- experiments/experiment8_ntk_clusters.py
+++ experiments/experiment8_ntk_clusters.py
@@ -7,6 +7,7 @@ from pytorch_lightning import LightningModule, Trainer
 from pytorch_lightning.callbacks import Callback
 import seaborn as sns
 from sklearn import cluster, metrics
+from statsmodels.stats.nonparametric import rank_compare_2indep
 import torch
 from typing import Any, Literal
 # local imports
@@ -19,13 +20,14 @@ from helpers import create_save_path
 parser = argparse.ArgumentParser("Run Neural Tangent Kernel experiments.")
 parser.add_argument("--nettype", type=str, default="ConvNet", choices=["ConvNet", "spcConvNet"])
 parser.add_argument("--shape", type=str, default="rectangles", choices=["rectangles", "LvT"])
+parser.add_argument("--pattern", type=str, default="color", choices=["color", "striped"])
 parser.add_argument("--eval_variant", type=str, default="standard", choices=["standard", "random"])
 parser.add_argument("--repetitions", type=int, default=10)
 parser.add_argument("--epochs", type=int, default=30)
 
-def run_cluster_analysis(args, pattern, variant, eval_data):
+def run_cluster_analysis(args, variant, eval_data):
     net = set_up_network(args.nettype)
-    data = make_dataset(args.shape, pattern, "small", variant)
+    data = make_dataset(args.shape, args.pattern, "small", variant)
     net = train_reference_net(net, data, args.epochs)
     eval_x, eval_y = next(iter(eval_data.test_dataloader()))
     ntk = get_ntk(net, eval_x)
@@ -58,41 +60,50 @@ def compare_clusters_to_labels(affinity_matrix, labels, n_cluster_range=range(2,
 def affinity_matrix_from_NTK(ntk):
     return torch.exp(ntk / ntk.std()).nan_to_num()
 
+def compare_maxima(data, args):
+    maxima = data.groupby(["NTK type", "repetition"]).max().reset_index()
+    shape_agreement = maxima[maxima["NTK type"] == "shape"].loc[:, "agreement"]
+    pattern_agreement = maxima[maxima["NTK type"] == args.patternname].loc[:, "agreement"]
+    result = rank_compare_2indep(pattern_agreement, shape_agreement)
+    return result
+
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    figpath = create_save_path("figures", "experiment_8", args.nettype, args.shape)
+    args.patternname = "color" if args.pattern == "color" else "texture"
+    figpath = create_save_path("figures", "experiment_8", args.nettype, args.shape, args.pattern)
 
     # clustering analysis for NTKs 
     n_cluster_range=range(2, 50)
     clustering_results = []
     for rep in range(args.repetitions):
         # set up evaluation data
-        shape_eval_data = make_dataset(args.shape, "color", "eval", "shapeonly", batchsize=4096)
-        color_eval_data = make_dataset(args.shape, "color", "small", "patternonly", batchsize=4096)
-        texture_eval_data = make_dataset(args.shape, "texture", "small", "patternonly", batchsize=4096)
+        eval_data = make_dataset(args.shape, args.pattern, "eval", "standard", batchsize=4096)
         # train networks
-        shape_agreement = run_cluster_analysis(args, "color", "shapeonly", shape_eval_data)
+        shape_agreement = run_cluster_analysis(args, "shapeonly", eval_data)
         clustering_results.append(
             pd.DataFrame(zip(shape_agreement, ["shape"] * len(shape_agreement), [rep] * len(shape_agreement)),
                          columns=["agreement", "NTK type", "repetition"])
         )
-        color_agreement = run_cluster_analysis(args, "color", "patternonly", color_eval_data)
+        pattern_agreement = run_cluster_analysis(args, "patternonly", eval_data)
         clustering_results.append(
-            pd.DataFrame(zip(color_agreement, ["color"] * len(color_agreement), [rep] * len(color_agreement)),
-                         columns=["agreement", "NTK type", "repetition"])
-        )
-        texture_agreement = run_cluster_analysis(args, "striped", "patternonly", texture_eval_data)
-        clustering_results.append(
-            pd.DataFrame(zip(texture_agreement, ["texture"] * len(texture_agreement), [rep] * len(texture_agreement)),
+            pd.DataFrame(zip(pattern_agreement, [args.patternname] * len(pattern_agreement), [rep] * len(pattern_agreement)),
                          columns=["agreement", "NTK type", "repetition"])
         )
     clustering_results = pd.concat(clustering_results)
+    clustering_results.to_csv(figpath + "/cluster_agreement.csv")
     # plot results
     plt.clf()
     sns.lineplot(clustering_results, x=clustering_results.index, y="agreement", hue="NTK type")
     plt.xlabel("number of clusters")
-    plt.title("NTK clustering on " + args.shape)
+    plt.title("NTK clustering on " + args.pattern + " " + args.shape)
     plt.savefig(figpath + "/cluster_analysis.png", bbox_inches="tight")
     plt.clf()
+    # significance test
+    test_result = compare_maxima(clustering_results, args)
+    with open(figpath + "/rank_comparison.txt", "w") as f:
+        f.write(f"Result of comparing maximum agreement between labels and {args.pattern} NTK clusters to agreement between labels and {args.shape} NTK clusters.\n\n")
+        f.write(str(test_result.summary()))
+        f.write("\n\n")
+        f.write("Full statistics:\n" + str(test_result))
