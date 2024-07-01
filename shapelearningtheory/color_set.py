8b92e094b6ff19e10cf2f7025e44744be5e9745a
Christian Jarvers <christian.jarvers@gmx.de>
2024-07-01 20:19:56 +0200

    Add parametrizable set of color classes.

---
+++ shapelearningtheory/color_set.py
@@ -0,0 +1,24 @@
+import colorsys
+from math import floor
+import numpy as np
+
+class ColorSet:
+    def __init__(self, number_of_classes: int, hues_per_class: int = 2):
+        self.number_of_classes = number_of_classes
+        self.hues_per_class = hues_per_class
+        self.number_of_hues = (number_of_classes * hues_per_class)
+        hues = np.arange(0, 1, 1 / self.number_of_hues)
+        np.random.shuffle(hues)
+        self.base_hues = np.split(hues, number_of_classes)
+
+    def sample(self, class_index):
+        base_hues_for_class = self.base_hues[class_index]
+        base_hue = np.random.choice(base_hues_for_class)
+        hue = base_hue + (np.random.rand() - 0.5) / self.number_of_hues
+        saturation = np.random.rand() / 2 + 0.5
+        intensity = np.random.rand() / 2 + 0.5
+        r, g, b = colorsys.hsv_to_rgb(hue, saturation, intensity)
+        return float_to_int_color(r), float_to_int_color(g), float_to_int_color(b)
+
+def float_to_int_color(f: float):
+    return floor(f * 255)
